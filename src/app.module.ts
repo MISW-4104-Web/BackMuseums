2c0927ba02ef56f6fccb335e13f6ee3b83487ee2
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2022-03-28 12:06:44 -0500

    Update collections

--- src/app.module.ts
+++ src/app.module.ts
@@ -38,22 +38,35 @@ import { Image } from './image/image.entity';
       type: 'postgres',
       host: process.env.DATABASE_URL && process.env.DATABASE_URL.replace('postgres://','').split(':')[1].split('@')[1] || process.env.DATABASE_URL || process.env.DB_HOST || 'localhost',
       port: 5432,
-      username: process.env.DATABASE_URL && process.env.DATABASE_URL.replace('postgres://','').split(':')[0] || process.env.DB_USER || 'postgres',
-      password: process.env.DATABASE_URL && process.env.DATABASE_URL.replace('postgres://','').split(':')[1].split('@')[0] || process.env.DB_PASSWORD || 'postgres',
-      database: process.env.DATABASE_URL && process.env.DATABASE_URL.split('/')[3] || process.env.DB_NAME || 'modern-art-museum',
+      username:
+        (process.env.DATABASE_URL &&
+          process.env.DATABASE_URL.replace('postgres://', '').split(':')[0]) ||
+        process.env.DB_USER ||
+        'postgres',
+      password:
+        (process.env.DATABASE_URL &&
+          process.env.DATABASE_URL.replace('postgres://', '')
+            .split(':')[1]
+            .split('@')[0]) ||
+        process.env.DB_PASSWORD ||
+        'postgres',
+      database:
+        (process.env.DATABASE_URL && process.env.DATABASE_URL.split('/')[3]) ||
+        process.env.DB_NAME ||
+        'modern-art-museum',
       entities: [Artist, Artwork, Exhibition, Image, Movement, Museum, Sponsor],
       dropSchema: true,
       synchronize: true,
       keepConnectionAlive: true,
       migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-      migrationsRun: false,
+      migrationsRun: true,
       cli: {
         migrationsDir: "src/migrations"
       },
       extra: {
-        /*ssl: {
+        ssl: {
           rejectUnauthorized: false
-        }*/
+        }
       }
     }),
   ],
